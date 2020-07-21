04fb975d4b4123975de25c3d55a2ac439656d812
Jose L Gonzalez <joseluisgonl@gmail.com>
2020-07-20 21:40:47 -0400

    Creating the generator for Portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_011852) do
+ActiveRecord::Schema.define(version: 2020_07_21_013516) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "tittle"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
