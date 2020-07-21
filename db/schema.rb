72fee7846d09150941821a9b5bb60298585a2a45
Jose L Gonzalez <joseluisgonl@gmail.com>
2020-07-20 21:29:19 -0400

    creating skills for the app

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_004611) do
+ActiveRecord::Schema.define(version: 2020_07_21_011852) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "tittle"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
